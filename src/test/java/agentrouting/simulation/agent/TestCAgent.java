e4e898cf57da18abb9966ce06fa3f1918559faaf
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-06-06 22:01:25 +0200

    create agent generator test

---
+++ src/test/java/agentrouting/simulation/agent/TestCAgent.java
@@ -0,0 +1,68 @@
+package agentrouting.simulation.agent;
+
+import agentrouting.simulation.CEnvironment;
+import agentrouting.simulation.IEnvironment;
+import agentrouting.simulation.algorithm.force.EForceFactory;
+import agentrouting.simulation.algorithm.routing.ERoutingFactory;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import org.lightjason.agentspeak.action.IAction;
+import org.lightjason.agentspeak.language.score.IAggregation;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+/**
+ * agent test
+ */
+public final class TestCAgent
+{
+    /**
+     * environment reference
+     */
+    private IEnvironment m_environment;
+    /**
+     * action references
+     */
+    private Set<IAction> m_actions;
+
+
+    /**
+     * data initialization
+     */
+    @Before
+    public void initialize()
+    {
+        m_environment = new CEnvironment( 100, 100, 25, ERoutingFactory.JPSPLUS.get() );
+
+        m_actions = Collections.unmodifiableSet( Stream.concat(
+            org.lightjason.agentspeak.common.CCommon.getActionsFromPackage(),
+            org.lightjason.agentspeak.common.CCommon.getActionsFromAgentClass( CMovingAgent.class )
+        ).collect( Collectors.toSet() ) );
+    }
+
+
+    /**
+     * test moving agent generator
+     * @throws Exception throws any exceptions
+     */
+    @Test
+    public void testMovingAgentGenerator() throws Exception
+    {
+        Assume.assumeNotNull( m_environment );
+        Assume.assumeNotNull( m_actions );
+
+        new CMovingAgentGenerator(
+            m_environment,
+            TestCAgent.class.getResourceAsStream( "/agentrouting/agent.asl" ),
+            m_actions,
+            IAggregation.EMPTY
+        ).generatesingle( EForceFactory.SUM.get(), "000000" );
+    }
+
+
+}

48a240a9e2ba6048d8e7c3e9bb54ed11df738ac1
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-08-18 18:07:18 +0200

    fix syntax

--- src/main/java/org/lightjason/examples/pokemon/simulation/agent/pokemon/CPokemon.java
+++ src/main/java/org/lightjason/examples/pokemon/simulation/agent/pokemon/CPokemon.java
@@ -28,11 +28,8 @@ import org.apache.commons.lang3.tuple.MutablePair;
 import org.lightjason.agentspeak.action.binding.IAgentAction;
 import org.lightjason.agentspeak.action.binding.IAgentActionFilter;
 import org.lightjason.agentspeak.beliefbase.CBeliefbasePersistent;
-import org.lightjason.agentspeak.beliefbase.IBaseBeliefbase;
 import org.lightjason.agentspeak.beliefbase.storage.CSingleStorage;
-import org.lightjason.agentspeak.beliefbase.storage.IBaseStorage;
 import org.lightjason.agentspeak.beliefbase.storage.IBeliefPerceive;
-import org.lightjason.agentspeak.beliefbase.view.IView;
 import org.lightjason.examples.pokemon.simulation.agent.EAccess;
 import org.lightjason.examples.pokemon.simulation.agent.IAgent;
 import org.lightjason.examples.pokemon.simulation.agent.IBaseAgent;
@@ -486,7 +483,7 @@ public final class CPokemon extends IBaseAgent
         @Override
         public final void perceive( final IAgent p_agent )
         {
-            p_agent.beliefbase().
+            //p_agent.beliefbase().
         }
     }
 
